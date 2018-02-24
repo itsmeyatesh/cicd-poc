ba68fd191981c46203f8a1271d9b0e0fa86a6b76
unknown <ybhargava@ITEM-90240.dhcp.noid.in.sopra>
2018-02-24 15:18:54 +0530

    initialize github repo

---
+++ src/main/java/com/sopra/ybh/greeting/GreetingServiceController.java
@@ -0,0 +1,24 @@
+package com.sopra.ybh.greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingServiceController {
+	
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method=RequestMethod.GET)
+    public @ResponseBody Greeting greeting(@RequestParam(value="name", defaultValue="Greetings") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+}
